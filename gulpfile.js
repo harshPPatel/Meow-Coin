a3cd5b58fa48a5ca8446061a323798033514eea8
harshPPatel <36562266+harshPPatel@users.noreply.github.com>
2018-11-13 08:52:41 -0600

    Features section markup is created!

--- gulpfile.js
+++ gulpfile.js
@@ -47,6 +47,9 @@ gulp.task('pug', function() {
 gulp.task('pugCopy', function () {
   return gulp.src('source/pug/*.html')
     .pipe(plumber())
+    .pipe(htmlmin({
+       collapseWhitespace: true
+    }))
     .pipe(gulp.dest(htmlDestination));
 })
 
